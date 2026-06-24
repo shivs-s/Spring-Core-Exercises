7412a90b5e32a053b1977c2e5c2ea5a346eb0a93
shivs-s <shiv16vani@gmail.com>
2026-06-24 19:41:22 +0530

    Added Spring Core Exercises

---
+++ src/main/java/com/library/service/BookService.java
@@ -0,0 +1,19 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+    public void setRepository(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void showService() {
+
+        System.out.println("Book Service Working");
+
+        repository.displayBook();
+    }
+}
